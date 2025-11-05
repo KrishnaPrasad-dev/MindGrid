ee1968cb4fb7d05511cf3f18ae60306e41d71da4
Krishna <itzmekrishna.257@gmail.com>
2025-11-05 22:26:15 +0530

    koncham frontend designing complete

--- frontend/MindGrid/src/sections/Login.jsx
+++ frontend/MindGrid/src/sections/Login.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import React from 'react';
+import Example from '../constants/EncryptButton';
+
 
 const Login = () => {
   return (
-    <div className='text-white mt-64'>
-        nice ra krishna nice saveeee
-    </div>
-  )
-}
 
-export default Login
+    <div className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden">
+      {/* BACKGROUND, covers viewport */}
+      <div className="fixed inset-0 w-screen h-screen bg-slate-950 z-0">
+        <div className="absolute bottom-0 left-[-20vw] top-[-10vh] h-[40vw] w-[40vw] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,0.15),rgba(255,255,255,0))]"></div>
+        <div className="absolute bottom-0 right-[-20vw] top-[-10vh] h-[40vw] w-[40vw] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,0.15),rgba(255,255,255,0))]"></div>
+      </div>
+
+      <div className='flex animate-text-gradient font-extrabold bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-3xl  text-transparent sm:text-5xl mt-32 items-center justify-center relative mx-auto text-center'>
+  Login
+</div>
+    
+
+      <div className="max-w-md w-full mx-auto border border-gray-300 dark:bg-gray-950 rounded-2xl mt-16 mb-24  p-8 bg-white bg-opacity-90 relative z-10">
+
+        
+
+
+        <form>
+          <div className="space-y-6">
+            <div>
+              <label className="text-slate-300 text-sm font-medium mb-2 block">Name</label>
+              <input
+                name="name"
+                type="text"
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter Your Name"
+              />
+            </div>
+
+            <div>
+              <label className="text-slate-300 text-sm font-medium mb-2 block">Email Id</label>
+              <input
+                name="email"
+                type="text"
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter email"
+              />
+            </div>
+
+            <div>
+              <label className="text-slate-300 text-sm font-medium mb-2 block">Password</label>
+              <input
+                name="password"
+                type="password"
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter password"
+              />
+            </div>
+
+            <div>
+              <label className="text-slate-300 text-sm font-medium mb-2 block">Roll Number</label>
+              <input
+                name="rollNumber"
+                type="text"
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter Your Roll Number"
+              />
+            </div>
+
+            
+
+              
+              
+            
+          </div>
+
+          <div className="mt-12">
+            
+            <Example buttonText="Login Securely" />          
+
+          </div>
+
+          <p className="text-slate-600 text-sm mt-6 text-center">
+            Already have an account?{' '}
+            <a href="javascript:void(0);" className="text-blue-600 font-medium hover:underline ml-1">
+              Login here
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
