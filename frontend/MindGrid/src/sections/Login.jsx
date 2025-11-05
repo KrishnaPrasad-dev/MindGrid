dcc99cb12464874a3632fc0f0d8d10edb29ef58f
Krishna <itzmekrishna.257@gmail.com>
2025-11-05 17:11:21 +0530

    sai ram ide last iga push chesta tarvata frontend chesta iga start chesi

--- frontend/MindGrid/src/sections/Login.jsx
+++ frontend/MindGrid/src/sections/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='text-white mt-64'>
-        please broooo login cheeyava
+        please broooo login cheeyava please ra badkow
     </div>
   )
 }
