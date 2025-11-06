75accffcce3148703820d8ece8d7e8ef57e7f233
Krishna <itzmekrishna.257@gmail.com>
2025-11-06 21:23:40 +0530

    saiiiiii rammmmmm squareee i made it responsive to the mobile also brooo

--- frontend/MindGrid/src/constants/GlowingCard.jsx
+++ frontend/MindGrid/src/constants/GlowingCard.jsx
@@ -3,7 +3,7 @@ import clubpic from '../assets/clubimg.jpg'
 
 const GlowingCard = ({ title, description }) => {
   return (
-    <div className="relative items-center  p-6 h-74 mx-4 w-84 rounded-xl mb-5 bg-gray-900 shadow-lg overflow-hidden cursor-pointer group max-w-md mx-auto mt-6">
+    <div className="relative items-center  p-4 h-74 mx-4 w-84 rounded-xl mb-5 bg-gray-900 shadow-lg overflow-hidden cursor-pointer group max-w-md mx-[20px] mt-6">
       <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 opacity-25 blur-lg group-hover:opacity-100 transition duration-1000" />
       <div className="relative z-10 text-white text-center">
         <h3 className="text-2xl font-bold mb-1">{title}</h3>
