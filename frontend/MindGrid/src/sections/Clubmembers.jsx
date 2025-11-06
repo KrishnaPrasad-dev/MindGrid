581e46d4c147fa81b2e7c679f1514717fb76bbad
Krishna <itzmekrishna.257@gmail.com>
2025-11-06 22:03:27 +0530

    did changes to the clubmembers

--- frontend/MindGrid/src/sections/Clubmembers.jsx
+++ frontend/MindGrid/src/sections/Clubmembers.jsx
@@ -31,108 +31,19 @@ const Clubmembers = () => {
           </h5>
 
           <ul className="my-4 space-y-3">
+
             <li className="relative">
               <div className="flex items-center justify-between p-3 font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white cursor-pointer">
                 <p className="ml-5">Krishna Prasad</p>
-
                 <div>
-
                 </div>
-                <button
-                  id="dropdownDefaultButton"
-                  type="button"
-                  onClick={toggleDropdown}
-                  className="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
+                <button className='text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
                   President
                   <img src={crown} className='ml-2 h-4 w-4' alt="crown" />
-                  <svg
-                    className="w-2.5 h-2.5 ms-3"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 1 4 4 4-4"
-                    />
-                  </svg>
                 </button>
-              </div>
-
-              <div
-                id="dropdown"
-                className={`absolute right-0 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 z-10 ${
-                  dropdownOpen ? '' : 'hidden'
-                }`}
-              >
-                <ul
-                  className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownDefaultButton"
-                >
-                 <li>
-
-            <a
-            href="#"
-            className="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-            <span>Vice-President</span>
-            <img src={crownvp} className=" ml-4  h-5 w-5" alt="crown" />
-            </a>
-                </li>
-
-                  <li>
-
-            <a
-            href="#"
-            className="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-            <span>Secretary</span>
-            <img src={cmpwrk} className=" ml-12 h-5 w-5" alt="crown" />
-            </a>
-                </li>
-
-
-            <li>
-
-            <a
-            href="#"
-            className="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-            <span>Treasurer</span>
-            <img src={treasurer} className="ml-12 h-5 w-5" alt="crown" />
-            </a>
-                </li>
-
-            <li>
-
-            <a
-            href="#"
-            className="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-            <span>Eliminate </span>
-            <img src={remove} className="ml-12  h-5 w-5" alt="crown" />
-            </a>
-                </li>
-
-
-                </ul>
               </div>
             </li>
             
-
-            
-
-
-
-
-
-
-
           </ul>
         </div>
       </div>
