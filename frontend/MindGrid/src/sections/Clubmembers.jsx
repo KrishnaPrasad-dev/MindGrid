29b6699f5574982853d08fe4f5237e533af53e59
Krishna Prasad <itzmekrishna.257@gmail.com>
2025-11-07 09:32:08 +0530

    Update Clubmembers.jsx

--- frontend/MindGrid/src/sections/Clubmembers.jsx
+++ frontend/MindGrid/src/sections/Clubmembers.jsx
@@ -34,7 +34,7 @@ const Clubmembers = () => {
 
             <li className="relative">
               <div className="flex items-center justify-between p-3 font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white cursor-pointer">
-                <p className="ml-5">Krishna Prasad</p>
+                <p className="ml-2 mr-2">Krishna Prasad</p>
                 <div>
                 </div>
                 <button className='text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
