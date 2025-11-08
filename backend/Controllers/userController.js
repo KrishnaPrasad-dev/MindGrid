17629bf58d31a3235e1bbd67a1da9783e3d726c7
Krishna <itzmekrishna.257@gmail.com>
2025-11-08 23:14:13 +0530

    fix: Vercel backend handler export

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,10 @@
+import User from "../models/User.js";
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}, "name role rollNumber email"); // exclude password
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
