ae67e9a2b1e1972bb4e7da9d738292e8f98080fc
Krishna <itzmekrishna.257@gmail.com>
2025-11-08 23:48:46 +0530

    fix: proper CORS setup for Vercel frontendaaaaaaaa

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -3,6 +3,6 @@ import { getAllUsers } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/all", getAllUsers);
+router.get("/all", getAllUsers);    
 
 export default router;
