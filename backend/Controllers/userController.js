5df40b09416b6a339597afe8c59383d83112f720
Krishna <itzmekrishna.257@gmail.com>
2025-11-10 00:31:03 +0530

    hello raa

--- backend/Controllers/userController.js
+++
@@ -1,5 +0,0 @@
-// controllers/userController.js
-// not used right now, safe placeholder
-exports.getAllUsers = async (req, res) => {
-  return res.status(501).json({ message: 'Users controller not implemented' });
-};
