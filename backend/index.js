28ea5263eb607798ddaf627f078ffa4522a1a344
Krishna <itzmekrishna.257@gmail.com>
2025-11-09 13:24:42 +0530

    500 error gone solving 503 error

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const UserRouter = require('./Routes/UserRouter')
+const AuthRouter = require('./Routes/AuthRouter')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
