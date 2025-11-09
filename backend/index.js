60ac4ede7627049fb686995ed06835e90dd33eb5
Krishna <itzmekrishna.257@gmail.com>
2025-11-09 13:49:32 +0530

    index js change chsina

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.MONGO_CONN || 8080;
 
 // ===== SAFE CORS =====
 const allowedOrigins = [
