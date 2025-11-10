3b6d5f6c504e7d0d31d94258b14f9836bb3f4235
Krishna <itzmekrishna.257@gmail.com>
2025-11-10 23:19:57 +0530

    no errors and added text white to role

--- frontend/MindGrid/src/sections/Profile.jsx
+++ frontend/MindGrid/src/sections/Profile.jsx
@@ -233,7 +233,7 @@ const Profile = (props) => {
                     <h1 className="text-2xl md:text-4xl font-extrabold text-white">{user.name}</h1>
                     <p className="mt-3 text-indigo-300 font-medium text-lg md:text-xl">{user.title}</p>
                     <div className="mt-5 inline-flex h-12 items-center justify-center rounded-md  font-medium text-white">
-                      <button className='inline-flex h-12 animate-background-shine items-center justify-center rounded-md border border-gray-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-gray-400 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50'>
+                      <button className='inline-flex text-white text-xl h-12 animate-background-shine items-center justify-center rounded-md border border-gray-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-gray-400 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50'>
       {user.role}
                       <img src={mem} alt="group" className="ml-2 w-9 h-10" />
     </button>
