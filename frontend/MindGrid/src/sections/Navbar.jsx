678f1f2a10aab8bf4813cd4d1972fdf360256790
Krishna <itzmekrishna.257@gmail.com>
2025-11-11 17:59:22 +0530

    mindgrid logo to the hero using mindgrid logo

--- frontend/MindGrid/src/sections/Navbar.jsx
+++ frontend/MindGrid/src/sections/Navbar.jsx
@@ -74,7 +74,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-3  c-space">
           <a
-            href="/"
+            href="/hero"
             className="text-neutral-400 font-bold hover:text-white transition-colors inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-xl text-transparent "
           >
             MindGrid
